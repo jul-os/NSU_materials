37d1ed0b17c0db969e629935a879ab0fa59a5e39
Julia Osnovskaia <78608364+jul-os@users.noreply.github.com>
2023-12-18 11:35:37 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
-#include "naming.h"
+#include "BitArray.h"
 #include <gtest/gtest.h>
 
-int main(int argc, char* argv[]) {
-    testing::InitGoogleTest(&argc, argv);
+// g++ -o main main.cpp BitArray.cpp && ./main
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
